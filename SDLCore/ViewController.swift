b70a59b8805a9cea7c7557d61e23aa1eaf5d4c57
Michael Pitts <gnxclone@gmail.com>
2017-08-25 19:48:52 -0400

    Initial Commit

---
+++ SDLCore/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SDLCore
+//
+//  Created by Michael Pitts on 8/25/17.
+//  Copyright © 2017 Xevo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
