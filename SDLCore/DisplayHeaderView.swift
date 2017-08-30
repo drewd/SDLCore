445e179277b94dd6087015f2036bcfb6b205d42d
Michael Pitts <gnxclone@gmail.com>
2017-08-30 16:35:07 -0400

    Added header to main display with working wall time

---
+++ SDLCore/DisplayHeaderView.swift
@@ -0,0 +1,9 @@
+//
+//  DisplayHeaderView.swift
+//  SDLCore
+//
+//  Created by Michael Pitts on 8/30/17.
+//  Copyright © 2017 Xevo. All rights reserved.
+//
+
+import Foundation
---
+++ SDLCore/ImageViewWithText.swift
@@ -0,0 +1,9 @@
+//
+//  ImageViewWithText.swift
+//  SDLCore
+//
+//  Created by Michael Pitts on 8/30/17.
+//  Copyright © 2017 Xevo. All rights reserved.
+//
+
+import Foundation
